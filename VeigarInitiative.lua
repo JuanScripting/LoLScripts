951adb8de8ddc6c335920f77b259be461ea425e7
JuanScripting <juanscripting@gmail.com>
2017-03-20 21:08:14 -0300

    Update VeigarInitiative.lua
    Variable name bug fixed

--- VeigarInitiative.lua
+++ VeigarInitiative.lua
@@ -3,7 +3,7 @@ if GetObjectName(GetMyHero()) ~= "Veigar" then return end
 local version = "0.9"
 
 function AutoUpdate(data)
-    if tonumber(data) > tonumber(ver) then
+    if tonumber(data) > tonumber(version) then
         print("New version found! " .. data)
         print("Downloading update, please wait...")
         DownloadFileAsync("https://raw.githubusercontent.com/JuanScripting/LoLScripts/master/VeigarInitiative.lua", SCRIPT_PATH .. "VeigarInitiative.lua", function() print("Update Complete, please 2x F6!") return end)
